f6922656f5c3baedae831ad9e522998a47b9985b
Zach Ferland <zachferland@gmail.com>
2013-07-25 14:15:05 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timecapsule::Application.config.secret_token = 'd9616333a0e19cac1bc6962b18e8dd49182350ae03983b76d91e995b5dd3c6424417abbea4d45fd0fc012102b294859901b25989ac7033851ba9ac29260e9924'
