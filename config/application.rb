dbe0ed973078ccab85f46334ea4ef6bd58dd2ecc
Zach Ferland <zachferland@gmail.com>
2013-09-05 16:08:09 +0200

    app

--- config/application.rb
+++ config/application.rb
@@ -46,8 +46,6 @@ module Timecapsule
     # Enable the asset pipeline
     config.assets.enabled = true
 
-    config.assets.initialize_on_precompile = false
-
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
