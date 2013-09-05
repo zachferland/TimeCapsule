52f1e1c7a944b218e800f5d52177b334b5b6ce6c
Zach Ferland <zachferland@gmail.com>
2013-09-05 14:54:36 +0200

    Clean migrations

--- db/migrate/20130812164829_add_uid_to_articles.rb
+++
@@ -1,5 +0,0 @@
-# class AddUidToArticles < ActiveRecord::Migration
-#   def change
-#     add_column :articles, :uid, :integer
-#   end
-# end
--- db/migrate/20130817164158_remove_uid_from_articles.rb
+++
@@ -1,9 +0,0 @@
-# class RemoveUidFromArticles < ActiveRecord::Migration
-#   def up
-#     remove_column :articles, :uid
-#   end
-
-#   def down
-#     add_column :articles, :uid, :integer
-#   end
-# end
