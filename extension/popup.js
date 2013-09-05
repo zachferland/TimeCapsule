c0df5e6df6ad8e003f9e5e925bb3dc9c850b8563
Zach Ferland <zachferland@gmail.com>
2013-09-06 01:45:41 +0200

    Add Mixpanel tracking

--- extension/popup.js
+++ extension/popup.js
@@ -1,3 +1,13 @@
+ // start Mixpanel 
+(function(e,b){if(!b.__SV){var a,f,i,g;window.mixpanel=b;a=e.createElement("script");a.type="text/javascript";a.async=!0;a.src='https://cdn.mxpnl.com/libs/mixpanel-2.2.min.js';f=e.getElementsByTagName("script")[0];f.parentNode.insertBefore(a,f);b._i=[];b.init=function(a,e,d){function f(b,h){var a=h.split(".");2==a.length&&(b=b[a[0]],h=a[1]);b[h]=function(){b.push([h].concat(Array.prototype.slice.call(arguments,0)))}}var c=b;"undefined"!==
+typeof d?c=b[d]=[]:d="mixpanel";c.people=c.people||[];c.toString=function(b){var a="mixpanel";"mixpanel"!==d&&(a+="."+d);b||(a+=" (stub)");return a};c.people.toString=function(){return c.toString(1)+".people (stub)"};i="disable track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config people.set people.set_once people.increment people.append people.track_charge people.clear_charges people.delete_user".split(" ");for(g=0;g<i.length;g++)f(c,i[g]);
+b._i.push([a,e,d])};b.__SV=1.2}})(document,window.mixpanel||[]);
+mixpanel.init("e36c53c122ec4bfa928d3501360c5f9a");
+ // end Mixpanel
+
+mixpanel.track('Open Extension');
+
+
 // GLOBAL
 var diffbotToken = "c71959b7b4f679b683a289a4d7dfa0bd";
 
@@ -28,11 +38,14 @@ var time = {
     //allows only one click, if rails i have to add it again
 		$(".time-val").one('click', function() { 
       $(".error").hide();
+
       // Show saving.... update
       // $(".progress").animate({height: "100px"}, 500);
       $(".loading").slideDown('slow');
 
 			var timevalue = parseInt($(this).data("value")); 
+
+      mixpanel.track('Send Article', {"Send Time": timevalue});
 
       console.log(timevalue);
 
