7ca322047a1701c6b5d63f0420bd1de01ce93787
Zach Ferland <zachferland@gmail.com>
2013-09-06 18:12:22 +0200

    Migrations provider uid to heroku

--- db/migrate/20130906160934_add_provider_and_uid_to_users.rb
+++ db/migrate/20130906160934_add_provider_and_uid_to_users.rb
@@ -1,8 +1,6 @@
 class AddProviderAndUidToUsers < ActiveRecord::Migration
   def change
     add_column :users, :provider, :string
-    add_column :users, :string, :string
     add_column :users, :uid, :string
-    add_column :users, :string, :string
   end
 end
